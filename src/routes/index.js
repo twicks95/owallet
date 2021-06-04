1eb011e3209886a17409ace7ea09a01563559d8f
twicks95 <teguh.wck95@gmail.com>
2021-06-04 11:34:30 +0700

    Add auth uri starting point

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const Route = express.Router()
+const authRouter = require('../modules/auth/auth_routes')
+
+Route.use('/auth', authRouter)
 
 module.exports = Route
