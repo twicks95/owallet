571701416f8ad6cfe6cebfd9ace487a51bf1aeee
twicks95 <teguh.wck95@gmail.com>
2021-07-14 18:07:50 +0700

    Update: index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const Route = express.Router()
 const authRouter = require('../modules/auth/auth_routes')
 const userRouter = require('../modules/user/user_routes')
+const transactionRouter = require('../modules/transaction/transaction_routes')
+const balanceRouter = require('../modules/balance/balance_routes')
+const topupRouter = require('../modules/topup/topup_routes')
 
 Route.use('/auth', authRouter)
 Route.use('/user', userRouter)
+Route.use('/balance', balanceRouter)
+Route.use('/transaction', transactionRouter)
+Route.use('/topup', topupRouter)
 
 module.exports = Route
